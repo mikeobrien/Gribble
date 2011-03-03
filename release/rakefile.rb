2641b7c8c64fe88b6cb3e3acd763e5d4d65e938d
Mike O'Brien <mob@mikeobrien.net>
2011-03-02 21:25:58 -0500

    Updates to the build script.

--- release/rakefile.rb
+++ release/rakefile.rb
@@ -1,6 +1,6 @@
 require "albacore"
 
-task :default => [:deploySample]
+task :default => [:unitTests]
 
 desc "Inits the build"
 task :initBuild do
@@ -10,56 +10,31 @@ desc "Generate assembly info."
 assemblyinfo :assemblyInfo => :initBuild do |asm|
     asm.version = ENV["GO_PIPELINE_LABEL"]
     asm.company_name = "Ultraviolet Catastrophe"
-    asm.product_name = "Wcf Rest Contrib"
-    asm.title = "Wcf Rest Contrib"
-    asm.description = "Goodies for Wcf Rest."
+    asm.product_name = "Gribble"
+    asm.title = "Gribble"
+    asm.description = "Gribble ORM"
     asm.copyright = "Copyright (c) 2010 Ultraviolet Catastrophe"
-    asm.output_file = "src/WcfRestContrib/Properties/AssemblyInfo.cs"
+    asm.output_file = "src/Gribble/Properties/AssemblyInfo.cs"
 end
 
 desc "Builds the library."
 msbuild :buildLibrary => :setAssemblyVersion do |msb|
-    msb.path_to_command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', 'v4.0.30319', 'MSBuild.exe')
     msb.properties :configuration => :Release
     msb.targets :Clean, :Build
-    msb.solution = "src/WcfRestContrib/WcfRestContrib.csproj"
+    msb.solution = "src/Gribble/Gribble.csproj"
 end
 
 desc "Builds the test project."
 msbuild :buildTestProject => :buildLibrary do |msb|
-    msb.path_to_command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', 'v4.0.30319', 'MSBuild.exe')
     msb.properties :configuration => :Release
     msb.targets :Clean, :Build
-    msb.solution = "src/WcfRestContrib.Tests/WcfRestContrib.Tests.csproj"
-end
-
-desc "Builds the sample app."
-msbuild :buildSampleApp => :buildTestProject do |msb|
-    msb.path_to_command = File.join(ENV['windir'], 'Microsoft.NET', 'Framework', 'v4.0.30319', 'MSBuild.exe')
-    msb.properties :configuration => :Release
-    msb.targets :Clean, :Build
-    msb.solution = "src/NielsBohrLibrary/NielsBohrLibrary.csproj"
-end
-
-desc "Set assembly reference in the sample project."
-task :addSampleAssemblyReference => :buildSampleApp do
-    path = "src/NielsBohrLibrary/NielsBohrLibrary.csproj"
-	replace = /<ProjectReference.*<\/ProjectReference>/m
-	reference = "<Reference Include=\"WcfRestContrib\"><HintPath>bin\WcfRestContrib.dll</HintPath></Reference>"
-    project = Common.ReadAllFileText(path)
-    project = project.gsub(replace, reference)
-    Common.WriteAllFileText(path, project) 
+    msb.solution = "src/Tests/Tests.csproj"
 end
 
 desc "NUnit Test Runner"
-nunit :unitTests => :buildInstaller do |nunit|
-	nunit.path_to_command = "lib/nunit/net-2.0/nunit-console.exe"
-	nunit.assemblies "src/WcfRestContrib.Tests/bin/Release/WcfRestContrib.Tests.dll"
+nunit :unitTests => :buildTestProject do |nunit|
+	nunit.path_to_command = "src/packages/NUnit.2.5.9.10348/Tools/nunit-console.exe"
+	nunit.assemblies "src/Tests/bin/Release/Tests.dll"
 	nunit.options "/xml=reports/TestResult.xml"
-end
-
-desc "Inits the deploy"
-task :initDeploy => :unitTests do
-    Common.EnsurePath(ReleasePath)
 end
 
