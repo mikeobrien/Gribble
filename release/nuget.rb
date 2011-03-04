d5c55325c7ffc0d869ad0cf92aa66717a90d8977
Mike O'Brien <mob@mikeobrien.net>
2011-03-04 18:36:43 -0500

    Adding nuget package creation to the build script.

--- release/nuget.rb
+++ release/nuget.rb
@@ -5,8 +5,7 @@ class NugetPush
 
     def run()
 		@apiKey = YAML::load(File.open(ENV["USERPROFILE"] + "/.nuget/credentials"))["api_key"] unless !@apiKey.nil?
-		puts @apiKey
-		system("nuget", "push", "-source", @package, @apiKey)
+		system("nuget", "push", @package, @apiKey)
     end
 end
 
