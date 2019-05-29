8b400f18625577f48b5d3b4de4970b20a0b823ca
mob <mob@mikeobrien.net>
2019-05-29 16:29:08 -0400

    Migrated guid comb from the windows api call.

--- src/Gribble/Extensions/GuidComb.cs
+++ src/Gribble/Extensions/GuidComb.cs
@@ -1,19 +1,32 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace Gribble.Extensions
 {
+    /// Via Jimmy Nilsson: http://www.informit.com/articles/article.asp?p=25862 
+
     public static class GuidComb
     {
-        [DllImport("rpcrt4.dll", SetLastError = true)]
-        private static extern int UuidCreateSequential(out Guid guid);
+        private static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
 
         public static Guid Create()
         {
-            Guid guid;
-            var result = UuidCreateSequential(out guid);
-            if (result == 0) return guid;
-            throw new Exception($"Error generating guid ({result}).");
+            var guidArray = Guid.NewGuid().ToByteArray();
+            var now = DateTime.UtcNow;
+            var days = new TimeSpan(now.Ticks - BaseDateTicks);
+            var msecs = now.TimeOfDay;
+            
+            // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333 
+            var daysArray = BitConverter.GetBytes(days.Days);
+            var msecsArray = BitConverter.GetBytes((long) (msecs.TotalMilliseconds / 3.333333));
+
+            // Reverse the bytes to match SQL Servers ordering 
+            Array.Reverse(daysArray);
+            Array.Reverse(msecsArray);
+            
+            Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
+            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+
+            return new Guid(guidArray);
         }
     }
 }
